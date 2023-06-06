dd94162033f3b7f4c59c6b3204d1570eae8bde9d
Chen <chen.jiang.1206@gmail.com>
2023-06-06 16:31:34 -0400

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./components/home/Home";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import ShoppingCart from "./components/shoppingcart/ShoppingCart";
+import Cakes from "./components/cakes/Cakes";
+import CakeCard from "./components/cakes/CakeCard";
+import Checkout from "./components/checkout/Checkout";
+import Error from "./components/error/Error";
+
 
 
 // rfce + tab
 // div + tab --> <div></div>
 // div. + tab --> <div className=""></div>
 // div.special --> <div className="special"></div>
-// div => main/section/article/etc. 
+// div => main/section/article/etc.
 
 function App() {
   return(
     <>
-      <Home />
+    <Nav />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/shoppingcart">
+        <ShoppingCart />
+      </Route>
+      <Route path="/cakes">
+        <Cakes />
+      </Route>
+      <Route path="/checkout">
+        <Checkout />
+      </Route>
+      <Redirect to="/error">
+        <Error />
+      </Redirect>
+
+    </Switch>
     </>
   )
 }
--- src/components/cakes/Cake.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Cake() {
-  return (
-    <div>Cake</div>
-  )
-}
-
-export default Cake
