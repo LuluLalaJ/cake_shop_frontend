5c778ddfdef3de9614bedd83b84e4b7b27a55ec2
Sarah Baughman <sarahbaughman92@gmail.com>
2023-06-07 09:38:22 -0400

    copied and pasted darkmode, added header and app

---
+++ src/components/app/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from './NavBar'
+import DarkMode from './DarkMode'
+
+function App() {
+    
+    return (
+        <div>
+            <Header/>
+            <DarkMode/>
+    
+            <div>Cake SHOP!</div>
+        </div>
+
+    )
+}
+
+export default App
