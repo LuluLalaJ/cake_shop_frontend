cdb73eb27916cec5852035146f31bf4aff264107
Sarah Baughman <sarahbaughman92@gmail.com>
2023-06-07 12:51:55 -0400

    rendered cakes

--- src/App.js
+++ src/App.js
@@ -20,15 +20,25 @@ import Error from "./components/error/Error";
 
 function App() {
 
+  const [cakes, setCakes] = useState([])
+
+  useEffect(() => {
+    fetch("/cakes")
+      .then((r) => r.json())
+      .then(cakes => setCakes(cakes))
+  }, []);
+  
+
+
   return(
     <Router>
       <Nav />
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route path="/about" component={About}/>
-        <Route path="/cakes" component={Cakes}/>
+        <Route path="/cakes"  render={(props) => <Cakes {...props} cakes = {cakes}/>} />
         <Route path="/shoppingcart" component={ShoppingCart}/>
-        <Route path="/checkout" component={Checkout}/>
+        <Route path="/checkout" component={Checkout} />
         <Route path="/login" component={Login}/>
         <Route path="/logout" component={Logout} />
         <Redirect to="/error" component={Error} />
--- src/components/cakes/Cakes.jsx
+++ src/components/cakes/Cakes.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState}  from 'react'
 import CakeCard from './CakeCard'
 
 function Cakes({cakes}) {
 
+  console.log(cakes)
+  
+  const renderCakes = cakes.map(cake =>
+        <CakeCard 
+          key = { cake.id }
+          cake = { cake }
+        />)
+    
+
   return(
-    <div>Cakes</div>
+    <div>
+      <h1>Cakes Page</h1>
+      {renderCakes}
+    </div>
   )
 }
 
-export default Cakes
+export default Cakes;
