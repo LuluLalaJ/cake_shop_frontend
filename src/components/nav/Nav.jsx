13dfed3cb6c700abfb8c2ef0f9dbd948c6ae87f2
Sarah Baughman <sarahbaughman92@gmail.com>
2023-06-07 09:20:41 -0400

    copy pasta navbar template

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,8 +2,42 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div>Nav</div>
-  )
+      <>
+        <NavLink className='nav-buttons'
+          to="/"
+          exact
+          style={linkStyles}
+          activeStyle={{ background: "brown" }}
+        >
+          Home
+        </NavLink>
+        <NavLink className='nav-buttons'
+          to="/discography"
+          exact
+          style={linkStyles}
+          activeStyle={{ background: "brown" }}
+        >
+          Discography
+        </NavLink>
+        <NavLink className='nav-buttons'
+          to="/schedule"
+          exact
+          style={linkStyles}
+          activeStyle={{ background: "brown" }}
+        >
+          Tour Schedule
+        </NavLink>
+  
+        <NavLink
+          to="/create" className='nav-buttons'
+          exact
+          style={linkStyles}
+          activeStyle={{ background: "brown" }}
+        >
+          Create
+        </NavLink>
+      </>
+    );
 }
 
 export default Nav
