11b4c928535e4bbe9d154ef1021e10cb277cd4b1
Sarah Baughman <sarahbaughman92@gmail.com>
2023-06-07 09:18:16 -0400

    added cake card figure template

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h1>About Us! About Cake SHOP!</h1>
+      <h1>About Cake SHOP!</h1>
       <p>Introducing Shen and Kara's Cake Shop, a delightful confectionary haven that sprang to life in 2023. Nestled in the heart of town, this whimsical bakery is where creativity, flavor, and artistry unite to redefine the world of cake-making.</p>
       <p>Shen and Kara, the visionary duo behind the cake shop, share an unwavering passion for crafting incredible cakes that are as visually stunning as they are delicious. With an artistic flair, they infuse their creations with an explosion of color, transforming each cake into a mesmerizing work of edible art. From vibrant rainbows to intricate designs, their cakes truly make a statement and leave a lasting impression on anyone lucky enough to indulge.</p>
       <p>But it's not just about the striking aesthetics. Shen and Kara understand that the flavors must match the artistry. Every cake that emerges from their ovens is a testament to their dedication in using only the finest ingredients and experimenting with unique flavor combinations. Each bite is a symphony of taste, an experience that lingers on the palate and sparks joy.</p>
--- src/components/cakes/CakeCard.jsx
+++ src/components/cakes/CakeCard.jsx
@@ -3,9 +3,24 @@ import React from 'react'
 function CakeCard() {
   return (
     <div>CakeCard
+      <Figure>
+        <Figure.Image
+          // className="cake-image"
+          // width={171}
+          // height={180}
+          // alt="171x180"
+          // src={cake image}
+          // onClick={() => displaySongs(id)}
+        />
+
+        <Figure.Caption>
+          {/* <h2>cake name, cake price, description?</h2> */}
+        </Figure.Caption>
+
+      </Figure>
     </div>
 
   )
 }
-
+// we need to import something to make this figure work, coming back to that 
 export default CakeCard
