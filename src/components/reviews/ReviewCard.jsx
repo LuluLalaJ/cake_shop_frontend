4ee2d4f53264f8525ecf70aa8d9f0490d435eb50
Chen <chen.jiang.1206@gmail.com>
2023-06-08 18:24:28 -0400

    add edit review to reviewpage

--- src/components/reviews/ReviewCard.jsx
+++ src/components/reviews/ReviewCard.jsx
@@ -2,14 +2,21 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { UserContext } from '../../context/UserContext';
 import { ReviewContext } from '../../context/ReviewContext';
+import ReviewForm from './ReviewForm';
 
 function ReviewCard({review}) {
 
   const { user } = useContext(UserContext);
   const {cake: {name}, content, user: {username}, id} = review
-  const { deleteReviewByReviewId } = useContext(ReviewContext)
+  const { deleteReviewByReviewId, editReviewByReviewId} = useContext(ReviewContext)
 
   const [editable, setEditable] = useState(false)
+  const [showEditForm, setShowEditForm] = useState(false);
+  function editReview(reviewId, content) {
+    editReviewByReviewId(reviewId, content)
+    setShowEditForm(false)
+ }
+
 
 
   //useEffect prevents infinite rerendering;
@@ -28,8 +35,17 @@ function ReviewCard({review}) {
         {editable
         ?
         <>
-            <button className="btn" onClick={null}>Edit my review</button>
             <button className="btn" onClick={()=>deleteReviewByReviewId(id)}>Delete my review</button>
+            <button className="btn" onClick={()=> setShowEditForm(!showEditForm)}>
+            {showEditForm
+            ? 'Close the form'
+            : 'Edit the review'}
+            </button>
+            {showEditForm && <ReviewForm
+              reviewId={id}
+              content={content}
+              submitReview={editReview}
+          />}
         </>
         : null}
     </div>
