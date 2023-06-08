72e156f1efffa86ee952a6ae742da1e0fa11fa56
Chen <chen.jiang.1206@gmail.com>
2023-06-08 13:54:04 -0400

    render past orders on mypage

--- src/components/mypage/MyPage.jsx
+++ src/components/mypage/MyPage.jsx
@@ -1,16 +1,56 @@
-import React, { useContext }from 'react';
+import React, { useContext, useEffect, useState }from 'react';
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import { UserContext } from '../../context/UserContext';
+import './mypage.css'
 
 function MyPage() {
   const { user, refresh} = useContext(UserContext);
+  const [ orders, setOrders ]= useState([])
+
+
+  useEffect( () => {
+    if (user) {
+      fetch(`/cakes/orders/${user.id}`)
+        .then(r => {
+        if (r.status === 200) {
+          r.json()
+          .then(data => setOrders(data))
+      }
+    })
+    }
+  }, [user])
+
+  const renderPastOrders = orders.map( order =>
+      <div key={order.id}>
+        <h3>- Order created: {order.created_at.slice(0, -3)}
+        </h3>
+        {order.order_cakes.map( (order, index) => <div key={index}>
+            <li className='past-order-info'>• Cake: {order.cake.name} |
+            Quantity: {order.quantity} </li>
+          </div>)}
+        <p> Total: $ {order.total_price}</p>
+      </div>
+
+
+  )
+
   if (!user) return (
     <Redirect to="/login" />
   )
 
   return (
     <div className="container">
-      Welcome back !
+      <h1>Welcome Back {user.username}!</h1>
+      <section className="order-conatiner">
+        <h2>Your past orders:</h2>
+        {orders.length === 0
+        ? <h3>You don't have any past orders!</h3>
+        : renderPastOrders
+        }
+      </section>
+      <section className="review-conatiner">
+        <h2>All your reviews:</h2>
+      </section>
     </div>
   )
 }
