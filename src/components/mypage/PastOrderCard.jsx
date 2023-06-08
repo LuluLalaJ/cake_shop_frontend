27cae776e0f46fe12a50daa263ecb29c8c5d1a32
Chen <chen.jiang.1206@gmail.com>
2023-06-08 16:08:06 -0400

    add submit review function

---
+++ src/components/mypage/PastOrderCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PastCakeCard from './PastCakeCard';
+
+function PastOrderCard({order}) {
+  
+  console.log('inside order card', order)
+    return (
+    <div>
+         {/* some bugs related to slice here - needs to be fixed */}
+        <h3>- Order created: {order.created_at}</h3>
+        {order.order_cakes.map( order => <PastCakeCard order={order}/>)}
+        <p> Total: $ {order.total_price}</p>
+    </div>
+    )
+}
+
+
+export default PastOrderCard
