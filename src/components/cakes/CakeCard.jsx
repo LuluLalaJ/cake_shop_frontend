41183300e9c86c28dea09c91ebb97ef1193e66ba
Sarah Baughman <sarahbaughman92@gmail.com>
2023-06-08 14:57:40 -0400

    added redirect to login to cart and favorite button

--- src/components/cakes/CakeCard.jsx
+++ src/components/cakes/CakeCard.jsx
@@ -4,15 +4,50 @@ import Figure from 'react-bootstrap/Figure'
 import './cakes.css'
 import { CartContext } from '../../context/CartContext'
 import { FavoriteContext } from '../../context/FavoriteContext'
+import { UserContext } from '../../context/UserContext';
 
 
 function CakeCard({cake}) {
   const { addToCart } = useContext(CartContext);
   const { addFavoriteCake } = useContext(FavoriteContext);
   const [isShort, setIsShort] = useState(true)
+  const { user } = useContext(UserContext);
 
   const {id, image, name, price, description} = cake
   
+
+  if (!user) {
+    return (
+      <Figure className="cake-card">
+        <Figure.Image
+          src={image}
+          className='cake-image'
+          />
+        <Figure.Caption className='cake-info'>
+          <h2>{name} </h2>
+          <h3>$ {price} </h3>
+          {isShort
+          ? <p>{description.slice(0,150)}... </p>
+          : <p>{description}</p>
+          }
+          <button onClick={()=>setIsShort(!isShort)} className='btn'>
+            {isShort
+            ? "Read more"
+            : "Read less"}
+          </button>
+  
+        </Figure.Caption>
+        <br></br>
+  
+        <Link to="/login"><button className='btn'>Add to Cart</button></Link>
+        <Link to="/login"><button className='btn' >Add to Favorites</button></Link>
+        <br/>
+        <h3><Link to={`/reviews/${cake.id}`}>Reviews</Link></h3>
+  
+      </Figure>
+    )
+  }
+
   return (
     <Figure className="cake-card">
       <Figure.Image
