02d1adf03c459eebbc474acd746b0f3b60ff70c9
Chen <chen.jiang.1206@gmail.com>
2023-06-09 10:33:39 -0400

    add alert

--- src/components/cakes/CakeCard.jsx
+++ src/components/cakes/CakeCard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import Figure from 'react-bootstrap/Figure'
 import { CartContext } from '../../context/CartContext'
@@ -10,10 +10,38 @@ import './cakes.css'
 function CakeCard({cake}) {
   const { addToCart } = useContext(CartContext);
   const { addFavoriteCake } = useContext(FavoriteContext);
-  const [isShort, setIsShort] = useState(true)
+  const [isShort, setIsShort] = useState(true);
+  const [addToCartAlert, setAddToCartAlert] = useState(false)
+  const [addToFavAlert, setAddToFavAlert] = useState(false)
+
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAddToCartAlert(false)
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [addToCartAlert])
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAddToFavAlert(false)
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [addToFavAlert])
 
   const {id, image, name, price, description} = cake
-  
+
+  function handleAddToCart(cake) {
+    addToCart(cake)
+    setAddToCartAlert(true)
+  }
+
+  function handleAddToFav(id) {
+    addFavoriteCake(id)
+    setAddToFavAlert(true)
+  }
+
+
   return (
     <Figure className="cake-card">
       <Figure.Image
@@ -36,12 +64,14 @@ function CakeCard({cake}) {
       </Figure.Caption>
       <br></br>
 
-      <button className='btn' onClick={(e)=>addToCart(cake)}>Add to Cart</button>
-      <button className='btn' onClick = {() => addFavoriteCake(id)} >Add to Favorites</button>
+      <button className='btn' onClick={(e)=>handleAddToCart(cake)}>Add to Cart</button>
+      <button className='btn' onClick = {() => handleAddToFav(id)} >Add to Favorites</button>
       {/* <br></br>
       <br></br> */}
       <Link to = {`/reviews/${cake.id}`}><button className='btn' >Reviews</button></Link>
-    
+      {addToCartAlert && <p className='alert'>Added to cart</p>}
+      {addToFavAlert && <p className='alert'>Added to favorites</p>}
+
 
     </Figure>
   )
